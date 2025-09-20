e98ada71481f7cf2f5001a01068a7d940e978c3d
DenkingOfficial <shershnev.andrei@gmail.com>
2025-09-20 09:24:47 +0300

    Better UA

--- parser/parser.py
+++ parser/parser.py
@@ -35,10 +35,10 @@ class EncarAPI:
 
     def _setup_headers(self):
         headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Linux x86_64; ko-KR) AppleWebKit/603.16 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/601",
             "Accept": "application/json, text/javascript, */*; q=0.01",
             "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Language": "Accept-Language: ko-KR,ko;q=0.9,en;q=0.8",
             "Cache-Control": "no-cache",
             "Origin": "https://www.encar.com",
             "Pragma": "no-cache",
